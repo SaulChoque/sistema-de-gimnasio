4409eb4dccf34ba0f6a680a69eda7cdabd1c258b
Yon <saulchoque123@gmail.com>
2026-05-25 11:20:54 -0400

    feat: actualizar mensajes de respuesta para reflejar el nombre del sistema de gestión de gimnasios

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,7 +20,7 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Sistema de gestion de Gimnasios');
   });
 
   afterEach(async () => {
