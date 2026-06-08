448fcd2803d3707d340b76474a492dd1f02bffa8
BaluChop <saulchoque123@gmail.com>
2026-06-07 21:31:28 -0400

    Add CORS origin for production frontend

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3001', 'https://sistema-de-gimnasio-front.vercel.app'],
     credentials: true,
   });
 
